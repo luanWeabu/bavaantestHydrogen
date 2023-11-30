3827299eaa601a931b14103d87efe1a19f051bf9
nguyenvanluan100720@gmail.com <nguyenvanluan100720@gmail.com>
2023-11-30 11:08:18 +0700

    replace Nextbutton

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -3,6 +3,7 @@ import useEmblaCarousel, {EmblaCarouselType} from 'embla-carousel-react';
 import Autoplay from 'embla-carousel-autoplay';
 import {Image} from '@shopify/hydrogen';
 import {NextButton, PrevButton} from './EmblaCarouselArrow';
+import {BannerItem} from '~/common/carouselType';
 
 export const EmblaCarousel = ({dataHomeSlider}: any) => {
   const [emblaRef, emblaApi] = useEmblaCarousel({loop: true}, [Autoplay()]);
@@ -16,9 +17,6 @@ export const EmblaCarousel = ({dataHomeSlider}: any) => {
     () => emblaApi && emblaApi.scrollNext(),
     [emblaApi],
   );
-  const dataImage = dataHomeSlider.map((item: any) => {
-    return item.image.data.attributes.url;
-  });
 
   const onSelect = useCallback((emblaApi: EmblaCarouselType) => {
     setPrevBtnDisabled(!emblaApi.canScrollPrev());
@@ -36,19 +34,14 @@ export const EmblaCarousel = ({dataHomeSlider}: any) => {
   return (
     <div className="embla" ref={emblaRef}>
       <div className="embla__container ">
-        {dataHomeSlider.map((item: any) => {
-          const dataImage = dataHomeSlider.map((item: any) => {
-            return item.image.data.attributes.url;
-          });
-          const dataImg = ['https://theme.truestorefront.com' + dataImage];
-
+        {dataHomeSlider.map((item: BannerItem) => {
           return (
             <div className="embla__slide relative" key={item.id}>
               <div className="absolute left-[10%] top-[30%] ">
                 <p className="text-[1.25rem] font-bold">{item.caption}</p>
                 <h4 className="text-[3.25rem] font-bold">
                   {item.title}
-                  {item.Line2 && item.Line2}
+                  {item.titleLine2 && item.titleLine2}
                 </h4>
                 <p className="font-bold mt-4 space leading-[1.333] max-w-[63ch]">
                   {item.text}
@@ -66,13 +59,12 @@ export const EmblaCarousel = ({dataHomeSlider}: any) => {
               </div>
               {item.image && (
                 <Image
-                  data={item.image.data.attributes.url}
                   aspectRatio="11/4"
-                  src={`https://theme.truestorefront.com${item.image.data.attributes.url}`}
+                  src={`https://theme.truestorefront.com${item?.image?.data?.attributes?.url}`}
                   sizes="(max-width: 32em) 100vw, 45vw"
                 />
               )}
-              <div className="embla__buttons flex justify-between w-[92vw]">
+              <div className="embla__buttons flex justify-between w-[97vw]">
                 <PrevButton onClick={scrollPrev} disabled={prevBtnDisabled} />
                 <NextButton onClick={scrollNext} disabled={nextBtnDisabled} />
               </div>
